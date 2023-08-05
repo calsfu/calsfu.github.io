c6235245fd19c266445cff54aa171bcdba88160c
coleres2 <colerezi30@gmail.com>
2023-08-04 17:41:03 -0700

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,290 @@
+<body>
+    <div class="main">
+        <div class="header">
+            <a>Cole Resurreccion</a>
+            <nav>
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Skills</a>
+                <a href="">Projects</a>
+            </nav>
+        </div>  
+        <div class="home">
+            <h1>Hello</h1>
+            <h4>Boston University Computer Engineering 25'</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class = "skills">
+            <h1>SKILLS</h1>
+            <p>Proficient</p>
+            <ul class="skillList">
+                <div class="poopy">
+                    <li class="wholeIcon">
+                        <div class="picture">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                        </div>
+                        <p class="skillText">C++</p>
+                    </li>
+                </div>
+                <!-- Repeat this for Verilog, MATLAB, Typescript, HTML, CSS, Svelte, Git, Rust -->
+                <div class="poopy">
+                    <li class="wholeIcon">
+                        <div class="picture">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                        </div>
+                        <p class="skillText">Verilog</p>
+                    </li>
+                </div>
+                <div class="poopy">
+                    <li class="wholeIcon">
+                        <div class="picture">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                        </div>
+                        <p class="skillText">MATLAB</p>
+                    </li>
+                </div>
+                <div class="poopy">
+                    <li class="wholeIcon">
+                        <div class="picture">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                        </div>
+                        <p class="skillText">Typescript</p>
+                    </li>
+                </div>
+                <div class="poopy">
+                    <li class="wholeIcon">
+                        <div class="picture">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                        </div>
+                        <p class="skillText">HTML</p>
+                    </li>
+                </div>
+                <div class="poopy">
+                    <li class="wholeIcon">
+                        <div class="picture">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                        </div>
+                        <p class="skillText">CSS</p>
+                    </li>
+                </div>
+                <div class="poopy">
+                    <li class="wholeIcon">
+                        <div class="picture">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                        </div>
+                        <p class="skillText">Git</p>
+                    </li>
+                </div>
+                <div class="poopy">
+                    <li class="wholeIcon">
+                        <div class="picture">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                        </div>
+                        <p class="skillText">Rust</p>
+                    </li>
+                </div>
+                <div class="poopy">
+                    <li class="wholeIcon">
+                        <div class="picture">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                        </div>
+                        <p class="skillText">Svelte</p>
+                    </li>
+                </div>
+            </ul>
+            <p>Learning</p>
+                <ul class="skillList">
+                    <div class="poopy">
+                        <li class="wholeIcon">
+                            <div class="picture">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                            </div>
+                            <p class="skillText">Python</p>
+                        </li>
+                    </div>
+                    <!-- Repeat this for Verilog, MATLAB, Typescript, HTML, CSS, Svelte, Git, Rust -->
+                    <div class="poopy">
+                        <li class="wholeIcon">
+                            <div class="picture">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                            </div>
+                            <p class="skillText">Algorithms</p>
+                        </li>
+                    </div>
+                    <div class="poopy">
+                        <li class="wholeIcon">
+                            <div class="picture">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                            </div>
+                            <p class="skillText">Data Structures</p>
+                        </li>
+                    </div>
+                    <div class="poopy">
+                        <li class="wholeIcon">
+                            <div class="picture">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                            </div>
+                            <p class="skillText">OS</p>
+                        </li>
+                    </div>
+                    <div class="poopy">
+                        <li class="wholeIcon">
+                            <div class="picture">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                            </div>
+                            <p class="skillText">Computer Architecture</p>
+                        </li>
+                    </div>
+                    <div class="poopy">
+                        <li class="wholeIcon">
+                            <div class="picture">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                            </div>
+                            <p class="skillText">Statistics</p>
+                        </li>
+                    </div>
+                    <div class="poopy">
+                        <li class="wholeIcon">
+                            <div class="picture">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                            </div>
+                            <p class="skillText">Machine Learning</p>
+                        </li>
+                    </div>
+                    <div class="poopy">
+                        <li class="wholeIcon">
+                            <div class="picture">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                            </div>
+                            <p class="skillText">Deep Learning</p>
+                        </li>
+                    </div>
+                    <div class="poopy">
+                        <li class="wholeIcon">
+                            <div class="picture">
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
+                            </div>
+                            <p class="skillText">Computer Vision</p>
+                        </li>
+                    </div>
+                </ul>
+                <!-- <li>Python</li>
+                <li>MongoDB</li>
+                <li>SQL</li>
+                <li>Algorithms</li>
+                <li>Data Structures</li>
+                <li>OS</li>
+                <li>Computer Architecture</li>
+                <li>Statistics</li>
+                <li>Machine Learning</li>
+                <li>Deep Learning</li>
+                <li>Computer Vision</li> -->
+        </div>
+    </div>
+    <div class="projects">
+        <h1>PROJECTS</h1>
+        <div class="">
+
+        </div>
+    </div>
+</body>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
+    /* section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+        text-align: center;
+    } */
+    body {
+        background-color: rgb(17, 17, 17);
+        
+        /* display: inline-block; */
+    }
+    a {
+        cursor: pointer;
+        text-decoration: none;
+        color: white;
+    }
+    .main { 
+        height: 100%;
+        width: 100%;
+        color : white;
+    }
+    .header {
+        font-family: 'Poppins', sans-serif;
+        font-size: 2rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        /* background-color: rgb(39, 39, 39); */
+        padding: 10px;
+        position: fixed;
+        z-index: 9999;
+    }
+    .header nav {
+        display: flex;
+        float: right;
+        color:white;
+        align-items: center;
+    }
+    .header nav a {
+        margin-left: 20px; /* Add spacing between navigation links */
+    }
+    .home {
+        font-family: 'Poppins', sans-serif;
+        font-size: 2rem;
+        text-align: center;
+    }
+    .home p {
+        font-size: 1rem;
+    }
+    .skills {
+        font-family: 'Poppins', sans-serif;
+        font-size: 2rem;
+        text-align: center;  
+        justify-content: center;
+    }
+    /* .skills ul {
+        
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+    } */
+    .skillList {
+        list-style-type: none;
+        display: grid;
+        /* flex-wrap: wrap; */
+        /* justify-content: center; */
+        align-items: center;
+        grid-template-columns: repeat(3, 16rem);
+        gap: 20px;
+        justify-content: center;
+    }
+    .wholeIcon {
+        display: grid;
+        grid-template-columns: max-content 1fr;
+        grid-template-rows: 1fr;
+        background-color: rgb(39, 39, 39);
+        /* display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center; */
+    }
+    .picture {
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        display: flex;
+        margin-left: 10px;
+    }
+    .skillText {
+        font-size: 1rem;
+        position: relative;
+        text-align: center;
+    }
+</style>
