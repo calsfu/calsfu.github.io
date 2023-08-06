f0155c0561b37c75202e12101e18c111bcbaafd0
coleres2 <colerezi30@gmail.com>
2023-08-06 16:18:21 -0700

    Updated look and color scheme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,31 @@
+<script>
+    let y = 50;
+    function scrollToHome() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+    function scrollToAbout() {
+        window.scrollTo({
+            top: 730,
+            behavior: 'smooth'
+        });
+    }
+    function scrollToSkills() {
+        window.scrollTo({
+            top: 1460,
+            behavior: 'smooth'
+        });
+    }
+    function scrollToProjects() {
+        window.scrollTo({
+            top: 2290,
+            behavior: 'smooth'
+        });
+    }
+</script>
+    <svelte:window bind:scrollY={y} />
 <body>
     <div class="main">
         <nav class="header">
@@ -7,7 +35,7 @@
                         <li>
                             <a href="">Cole Resurreccion</a>
                             
-                                <a href="https://github.com/calsfu"><img src="/src/lib/images/github-mark-white.png" alt="logo" class="logo"></a>
+                                <a href="https://github.com/calsfu" target="_blank" rel="noopener noreferrer"><img src="/src/lib/images/github-mark-white.png" alt="logo" class="logo"></a>
                             
                         </li>
                     </ul>
@@ -15,34 +43,31 @@
                 <li class="topRight">
                     <ul class="guide">
                         <li class="guideItem">
-                            <a href="">Home</a>
+                            <button on:click={scrollToHome}>Home</button>
                         </li>
                         <li class="guideItem">
-                            <a href="">About</a>
+                            <button on:click={scrollToAbout}>About</button>
                         </li>
                         <li class="guideItem"> 
-                            <a href="">Skills</a>
+                            <button on:click={scrollToSkills}>Skills</button>
                         </li>
                         <li class="guideItem">
-                            <a href="">Projects</a>
+                            <button on:click={scrollToProjects}>Projects</button>
                         </li>
                     </ul>
                 </li>
             </ul>
-            
-            <!-- <<li>>
-                <a href="">Home</a>
-                <a href="">About</a>
-                <a href="">Skills</a>
-                <a href="">Projects</a>
-            </nav> -->
         </nav>  
-        <div class="home">
-            <h1>Hello</h1>
+        <section id="home" class="home">
+            <h1>Hello, I'm Cole Resurreccion</h1>
             <h4>Boston University Computer Engineering 25'</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div class = "skills">
+        </section>
+        <section id="about" class="about">
+            <p>I am a third year computer engineering at Boston University. Currently, I am on a software track. I am learning C++ and Verilog, but am self-teaching myself
+                other front end languages. 
+            </p>
+        </section>
+        <section class = "skills">
             <h1>SKILLS</h1>
             <p>Proficient</p>
             <ul class="skillList">
@@ -100,7 +125,7 @@
                         <div class="picture">
                             <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"></path></svg>
                         </div>
-                        <p class="skillText">Git</p>
+                        <p class="skillText">Arduino</p>
                     </li>
                 </div>
                 <div class="poopy">
@@ -207,14 +232,20 @@
                 <li>Machine Learning</li>
                 <li>Deep Learning</li>
                 <li>Computer Vision</li> -->
-        </div>
+                </section>
         <section class="projects">
             <h1 class="projectTitle">PROJECTS</h1>
             <div class="projectBox">
                 <div class="textBox">
                     <h2>Model Processor</h2>
-                    <h3>Digital Logic</h3>
-                    <p>Lorem ispum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+                    <h4>Digital Logic</h4>
+                    <p>Created a model CPU with 8 registers and a 16-bit bus. Supports 8 functions lie, ADD, SUB, MULT, JUMP. Recieved information from ROM storage and a program conter. Supports FPGA.</p>
+                    <div class="projectInfo">
+                        <img src="/src/lib/images/verilog-svgrepo-com.svg" alt="verilog-symbol" id="verilog-symbol">
+                        <a href="https://github.com/calsfu/verilog_processor" target="_blank" rel="noopener noreferrer" id="seemorebutton">
+                            See More
+                        </a>
+                    </div>
                 </div>
                 <div class="imgBox">
                     <img src="/src/lib/images/ModelProcessor.png" alt="Model Processor" class="projectImg">
@@ -222,19 +253,32 @@
             </div>
             <div class="projectBox">
                 <div class="imgBox">
-                    <img src="/src/lib/images/ModelProcessor.png" alt="Model Processor" class="projectImg">
+                    <img src="/src/lib/images/BikeLightPhoto.png" alt="Bike Light" class="projectImg">
                 </div>
                 <div class="textBox">
-                    <h2>Model Processor</h2>
-                    <h3>Digital Logic</h3>
-                    <p>Lorem ispum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+                    <h2>Bike Light</h2>
+                    <h4>Microcontroller Intergration</h4>
+                    <p>A bike light that turns off in bright settings and gets brighter as speed increases. Collaborated in a group to create a circuit diagram, reasearch and purchas parts
+                        , and wrote code for an Arduino microcontroller.  </p>
+                    <div class="projectInfo">
+                        <img src="/src/lib/images/arduino-svgrepo-com.svg" alt="arduino-symbol" id="arduino-symbol">
+                        <a href="https://github.com/calsfu/verilog_processor" target="_blank" rel="noopener noreferrer" id="seemorebutton">
+                            See More
+                        </a>
+                    </div>
                 </div>
             </div>
             <div class="projectBox">
                 <div class="textBox">
-                    <h2>Model Processor</h2>
-                    <h3>Digital Logic</h3>
-                    <p>Lorem ispum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+                    <h2>ASCII Pokemon Game</h2>
+                    <h4>C++ Terminal Game</h4>
+                    <p>Pokemon game using C++ in the terminal. Uses classes, inheritance, polymorphism, and more. </p>
+                    <div class="projectInfo">
+                        <img src="/src/lib/images/c++-icon.svg" alt="cpp-symbol" id="cpp-symbol">
+                        <a href="https://github.com/calsfu/ASCII-Pokemon-Game" target="_blank" rel="noopener noreferrer" id="seemorebutton">
+                            See More
+                        </a>
+                    </div>
                 </div>
                 <div class="imgBox">
                     <img src="/src/lib/images/ModelProcessor.png" alt="Model Processor" class="projectImg">
@@ -247,18 +291,16 @@
 </body>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
-    /* section {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        min-height: 100vh;
-        text-align: center;
-    } */
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;700&display=swap');
     body {
         background-color: rgb(17, 17, 17);
-        
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: 0;
+        padding: 0;
+
         /* display: inline-block; */
     }
     a {
@@ -272,17 +314,17 @@
         color : white;
     }
     .header {
-        font-family: 'Poppins', sans-serif;
         font-size: 2rem;
         display: flex;
         justify-content: space-between;
         align-items: center;
-        background-color: rgb(39, 39, 39); 
+        background-color: rgb(0, 0, 39); 
         position: fixed;
         top: 0px;
         right: 0px;
         left: 0px;;
         z-index: 9999;
+        
     }
     .headerItems {
         display: flex;
@@ -292,13 +334,14 @@
         align-items: center;
         list-style-type: none;
         margin: auto;
+        
     }
     .guide {
         display: flex;
         flex-direction: row;
         align-items: center;
         list-style-type: none;
-        gap: 15px;
+        gap: 30px;
         padding-left: 0;
     }
     .logo {
@@ -309,13 +352,23 @@
         padding-left: 4px;
     }
     .guideItem {
-        font-family: 'Poppins', sans-serif;
-        font-size: 2rem;
+        font-size: 1.4rem;
         text-align: center;
         justify-content: center;
         text-decoration: none;
     }
+    .guideItem button {
+        background-color: rgb(0, 0, 39);
+        border: none;
+        color: white;
+        font-size: 1.4rem;
+        cursor: pointer;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 300;
+    }
     .topLeft {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 700;
         display: flex;
         flex-direction: row;
         align-items: center;
@@ -327,14 +380,31 @@
         justify-content: space-between;
         align-items: center;
         list-style-type: none;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 300;
+        font-size: .5rem;
     }
     .home {
         font-family: 'Poppins', sans-serif;
         font-size: 2rem;
         text-align: center; 
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
     }
-    .home p {
-        font-size: 1rem;
+    .about {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-family: 'Poppins', sans-serif;
+        font-size: 1.5rem;
+        text-align: center;
+        width: 90%;
+        height: 100vh;
+        margin: auto;
     }
     .skills {
         font-family: 'Poppins', sans-serif;
@@ -355,7 +425,7 @@
         /* flex-wrap: wrap; */
         /* justify-content: center; */
         grid-template-columns: repeat(3, 16rem);
-        gap: 20px;
+        gap: 25px;
         justify-content: center;
         padding-left: 0px;
     }
@@ -375,7 +445,7 @@
         position: relative;
         display: flex;
         padding: 7px;
-        background-color: gray;
+        background-color: rgb(70, 70, 70);
     }
     .skillText {
         font-size: 1rem;
@@ -400,11 +470,41 @@
         margin-bottom: 5%;
     }
     .textBox {
-        background-color: rgb(39, 39, 39);
-        height: 100%;
-       
-        text-align: center;
+        background-color: rgb(0, 0, 39);
+        height: 70%;
+        margin-top: 4%;
+        padding: 4%;
         position: relative;
+    }
+    .textBox h2 {
+        font-size: 2rem;
+        margin: 0px;
+        margin-top: 4%;
+    }
+    .textBox h4 {
+        font-size: 1.2rem;
+        margin: 0px;
+    }
+    .textBox p {
+        font-size: 1rem;
+    }
+    .projectInfo {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 100%;
+    }
+    #verilog-symbol {
+        width: 7%;
+        height: auto;
+    }
+    #arduino-symbol {
+        width: 7%;
+        height: auto;
+    }
+    #cpp-symbol {
+        width: 7%;
+        height: auto;
     }
     .imgBox {
         /* width: 40%;
@@ -416,7 +516,6 @@
         float:right;
         left: auto;
         right: 0px;
-        margin-top: 7%;
         height: auto;
         padding-top: 0px;
         overflow: hidden;
