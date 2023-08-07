8a416cf955e4e7b02f0eb9881ea078abee7efbd7
coleres2 <colerezi30@gmail.com>
2023-08-07 11:36:20 -0700

    Added dark/light mode toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     let y = 50;
+    let darkMode = true;
     function scrollToHome() {
         window.scrollTo({
             top: 0,
@@ -14,19 +15,31 @@
     }
     function scrollToSkills() {
         window.scrollTo({
-            top: 1460,
+            top: 1650,
             behavior: 'smooth'
         });
     }
     function scrollToProjects() {
         window.scrollTo({
-            top: 2290,
+            top: 2450,
             behavior: 'smooth'
         });
+    }
+    function toggleLightMode() {
+        darkMode = !darkMode;
+        if(darkMode) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+        }
     }
 </script>
 <header>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"> 
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Cole Resurreccion</title>
+
 </header>
     <svelte:window bind:scrollY={y} />
 <body>
@@ -35,9 +48,10 @@
             <ul class="headerItems">
                 <li class="topLeft">
                     <ul class="guide">
-                        <li>
+                        <li style="display:flex; flex-direction:row; align-items: center; justify-content:center;    ">
                             <button on:click={scrollToHome} id="mainBut">Cole Resurreccion</button>
                             <a href="https://github.com/calsfu" target="_blank" rel="noopener noreferrer"><img src="/src/lib/images/github-mark-white.png" alt="logo" class="logo"></a>
+                            <button on:click={toggleLightMode} tabindex="0" style="background-color:transparent; border:0px; cursor:pointer"><img style=" width:25px; height:auto; margin-top:7px;"src="/src/lib/images/moon-svgrepo-com.svg" alt="nightmode"></button>
                         </li>
                     </ul>
                 </li>
@@ -64,7 +78,7 @@
             <h4>Boston University Computer Engineering 25'</h4>
         </section>
         <section id="about" class="about">
-            <p>I am a third year computer engineering at Boston University. Currently, I am on a software track. I am learning C++ and Verilog, but am self-teaching myself
+            <p>I am a third year computer engineering at Boston University. Currently, I am on a software track and am learning C++, MATLAB, and Verilog in school, but am self-teaching myself
                 other front end languages. 
             </p>
         </section>
@@ -84,7 +98,7 @@
                 <div class="poopy">
                     <li class="wholeIcon">
                         <div class="picture">
-                            <img id="toobig" src="/src/lib/images/verilog-svgrepo-com.svg">
+                            <img id="toobig" alt="poopy" src="/src/lib/images/verilog-svgrepo-com.svg">
                         </div>
                         <p class="skillText">Verilog</p>
                     </li>
@@ -239,7 +253,7 @@
             <div class="projectBox">
                 <div class="textBox">
                     <h2>Model Processor</h2>
-                    <h4>Digital Logic</h4>
+                    <h4 >Digital Logic</h4>
                     <p>Created a model CPU with 8 registers and a 16-bit bus. Supports 8 functions lie, ADD, SUB, MULT, JUMP. Recieved information from ROM storage and a program conter. Supports FPGA.</p>
                     <div class="projectInfo">
                         <img src="/src/lib/images/verilog-svgrepo-com.svg" alt="verilog-symbol" id="verilog-symbol">
@@ -258,7 +272,7 @@
                 </div>
                 <div class="textBox">
                     <h2>Bike Light</h2>
-                    <h4>Microcontroller Intergration</h4>
+                    <h4 >Microcontroller Intergration</h4>
                     <p>A bike light that turns off in bright settings and gets brighter as speed increases. Collaborated in a group to create a circuit diagram, reasearch and purchas parts
                         , and wrote code for an Arduino microcontroller.  </p>
                     <div class="projectInfo">
@@ -272,25 +286,25 @@
             <div class="projectBox">
                 <div class="textBox">
                     <h2>ASCII Pokemon Game</h2>
-                    <h4>C++ Terminal Game</h4>
+                    <h4 >C++ Terminal Game</h4>
                     <p>Pokemon game using C++ in the terminal. Uses classes, inheritance, polymorphism, and more. </p>
                     <div class="projectInfo">
-                        <img src="/src/lib/images/c++-icon.svg" alt="cpp-symbol" id="cpp-symbol">
+                        <i class="devicon-cplusplus-plain-wordmark"></i>
                         <a href="https://github.com/calsfu/ASCII-Pokemon-Game" target="_blank" rel="noopener noreferrer" id="seemorebutton">
                             See More
                         </a>
                     </div>
                 </div>
                 <div class="imgBox">
-                    <img src="/src/lib/images/ModelProcessor.png" alt="Model Processor" class="projectImg">
+                    <img src="/src/lib/images/pokemon.png" alt="Model Processor" class="projectImg">
                 </div>
             </div>
-            <p>Find more <a href="https://github.com/calsfu?tab=repositories">here</a></p>
+            <p style="color: var(--text-color);">Find more <a href="https://github.com/calsfu?tab=repositories">here</a></p>
         </section>
         <section class="footer">
             <div class="footerInfo"> 
                 <a href="https://github.com/calsfu" target="_blank" rel="noopener noreferrer"><img src="/src/lib/images/github-mark-white.png" alt="logo" class="logo"></a>
-                <p>© 2023 Cole Resurreccion</p>
+                <p>Made by Cole Resurreccion</p>
                 <a href="mailto::business@gmail.com" target="_blank" rel="noopener noreferrer">Contact Me</a>
             </div>
         </section>
@@ -300,8 +314,29 @@
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;700&display=swap');
+    :root {
+        --background-color: rgb(17, 17, 17); 
+        --header-color: rgb(0, 0, 39)   ;
+        --colored-text: rgb(77, 139, 255);
+        --project-colored-text: rgb(77, 139, 255);
+        --text-color: white;
+        --white: white;
+        --skill-text: rgb(39, 39, 39);
+        --skill-icon: rgb(70, 70, 70);
+        
+    }
+    :root[data-theme='light'] {
+        --background-color: white; 
+        --header-color: rgb(111, 0, 255);
+        --colored-text: rgb(111, 0, 255);
+        --project-colored-text: white;
+        --text-color: black;
+        --white: white;
+        --skill-text: rgb(111, 0, 255);
+        --skill-icon: rgb(71, 0, 163);
+  }
     body {
-        background-color: rgb(17, 17, 17);
+        background-color: var(--background-color);
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -314,25 +349,25 @@
     .header a {
         cursor: pointer;
         text-decoration: none;
-        color: white;
+        color: var(--white);
     }
     .projectBox a {
         cursor: pointer;
         text-decoration: underline;
-        color: white;
+        color: var(--white);
         text-align: center;
     }
     .main { 
         height: 100%;
         width: 100%;
-        color : white;
+        color : var(--text-color);
     }
     .header {
         font-size: 2rem;
         display: flex;
         justify-content: space-between;
         align-items: center;
-        background-color: rgb(0, 0, 39); 
+        background-color: var(--header-color); 
         position: fixed;
         top: 0px;
         right: 0px;
@@ -365,12 +400,12 @@
         padding-left: 4px;
     }
     .color_name {
-        color: rgb(77, 139, 255);
+        color: var(--colored-text);
     }
     #mainBut {
-        background-color: rgb(0, 0, 39);
+        background-color: var(--header-color);
         border: none;
-        color: white;
+        color: var(--white);
         font-size: 1.5rem;
         cursor: pointer;
         font-family: 'Poppins', sans-serif;
@@ -383,9 +418,9 @@
         text-decoration: none;
     }
     .guideItem button {
-        background-color: rgb(0, 0, 39);
+        background-color: var(--header-color);
         border: none;
-        color: white;
+        color: var(--white);
         font-size: 1.4rem;
         cursor: pointer;
         font-family: 'Poppins', sans-serif;
@@ -436,6 +471,7 @@
         font-size: 2rem;
         text-align: center;  
         justify-content: center;
+        color: var(--text-color);
     }
     /* .skills ul {
         
@@ -448,6 +484,7 @@
         list-style-type: none;
         display: grid;
         /* flex-wrap: wrap; */
+        color: var(--white);
         /* justify-content: center; */
         grid-template-columns: repeat(3, 16rem);
         gap: 25px;
@@ -458,11 +495,15 @@
         display: grid;
         grid-template-columns: max-content 1fr;
         grid-template-rows: 1fr;
-        background-color: rgb(39, 39, 39);
+        background-color: var(--skill-text);
+        
         /* display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center; */
+    }
+    .wholeIcon:hover {
+        background-color: var(--header-color);
     }
     .picture {
         justify-content: center;
@@ -470,7 +511,7 @@
         position: relative;
         display: flex;
         padding: 7px;
-        background-color: rgb(70, 70, 70);
+        background-color: var(--skill-icon);
     }
     #toobig {
         width: 32px;
@@ -484,13 +525,14 @@
     .projects {
         font-family: 'Poppins', sans-serif;
         font-size: 2rem;
-        color : white;
+        color : var(--text-color);
         margin:5%;
     }
     .projectTitle {
         text-align: center;
     }
     .projectBox {
+        color :var(--white);
         display: grid;
         margin: 0%;
         width: 95%;
@@ -499,8 +541,8 @@
         margin-bottom: 5%;
     }
     .textBox {
-        background-color: rgb(0, 0, 39);
-        height: 70%;
+        background-color: var(--header-color);
+        height: fit-content;
         margin-top: 2%;
         padding: 4%;
         position: relative;
@@ -512,6 +554,7 @@
     .textBox h4 {
         font-size: 1.2rem;
         margin: 0px;
+        color: var(--project-colored-text);
     }
     .textBox p {
         font-size: 1rem;
@@ -558,10 +601,11 @@
     .footer {
         width: 100%;
         height: 50px;
-        background-color: rgb(0, 0, 39);
+        background-color: var(--header-color);
         font-family: 'Poppins', sans-serif;
         font-weight: 300;
         font-size: .6rem;
+        color:var(--white);
 
     }
     .footerInfo {
