94b6a90a48dd2afe8c2cd30ca723702535bfa5e0
coleres2 <colerezi30@gmail.com>
2024-01-21 19:08:21 -0500

    Edited skills to shorten code

--- src/routes/Skills.svelte
+++ src/routes/Skills.svelte
@@ -1,5 +1,74 @@
 <script>
     //will be used to show skills with loop
+    const knownSkills = [
+        {
+            name: 'C++',
+            icon: 'devicon-cplusplus-plain-wordmark'
+        },
+        {
+            name: 'Python',
+            icon: 'devicon-python-plain'
+        },
+        {
+            name: 'MATLAB',
+            icon: 'devicon-matlab-plain'
+        },
+        {
+            name: 'Typescript',
+            icon: 'devicon-typescript-plain'
+        },
+        {
+            name: 'Algorithms',
+            icon: 'devicon-thealgorithms-plain'
+        },
+        {
+            name: 'Data Structures',
+            icon: 'data-structure'
+        },
+        {
+            name: 'Arduino',
+            icon: 'devicon-arduino-plain'
+        },
+        {
+            name: 'Rust',
+            icon: 'devicon-rust-plain'
+        },
+        {
+            name: 'Svelte',
+            icon: 'devicon-svelte-plain'
+        },
+        {
+            name: 'Python',
+            icon: 'devicon-python-plain'
+        },
+        {
+            name: 'OS',
+            icon: 'operating-systems'
+        },
+        {
+            name: 'Statistics',
+            icon: 'statistics'
+        },
+    ];
+    const learningSkills = [
+        {
+            name: 'Computer Architecture',
+            icon: 'computer-arch'
+        },
+        
+        {
+            name: 'Machine Learning',
+            icon: 'machine-learning'
+        },
+        {
+            name: 'Deep Learning',
+            icon: 'deep-learning-project'
+        },
+        {
+            name: 'Computer Vision',
+            icon: 'eye'
+        }
+    ];
 </script>
 
 <style>
@@ -52,150 +121,45 @@
         height: 100vh;
         color: var(--text-color);
     }
+    i {
+        min-width: 32px;
+    }
 </style>
 <section class = "skills" id="skills">
     <h1>SKILLS</h1>
                 <p>Know</p>
                 <ul class="skillList">
-                    <div class="poopy">
-                        <li class="wholeIcon">
-                            <div class="picture">
-                                <i class="devicon-cplusplus-plain-wordmark"></i>
-                            </div>
-                            <p class="skillText">C++</p>
-                        </li>
-                    </div>
-                    <!-- Repeat this for Verilog, MATLAB, Typescript, HTML, CSS, Svelte, Git, Rust -->
-                    <div class="poopy">
-                        <!-- <li class="wholeIcon">
-                            <div class="picture">
-                                <img id="toobig" alt="poopy" src="/verilog-svgrepo-com.svg">
-                            </div>
-                            <p class="skillText">Verilog</p>
-                        </li> -->
-                        <li class="wholeIcon">
-                            <div class="picture">
-                                <i class="devicon-python-plain"></i>
-                            </div>
-                            <p class="skillText">Python</p>
-                        </li>
-                    </div>
-                    <div class="poopy">
-                        <li class="wholeIcon">
-                            <div class="picture">
-                                <i class="devicon-matlab-plain"></i>
-                            </div>
-                            <p class="skillText">MATLAB</p>
-                        </li>
-                    </div>
-                    <div class="poopy">
-                        <li class="wholeIcon">
-                            <div class="picture">
-                                <i class="devicon-typescript-plain"></i>
-                            </div>
-                            <p class="skillText">Typescript</p>
-                        </li>
-                    </div>
-                    <div class="poopy">
-                        <li class="wholeIcon">
-                            <div class="picture">
-                                <i class="devicon-thealgorithms-plain"></i>
-                            </div>
-                            <p class="skillText">Algorithms</p>
-                        </li>
-                    </div>
-                    <div class="poopy">
-                        <li class="wholeIcon">
-                            <div class="picture">
-                                <img src="/data-structure.svg" alt="datas" id="toobig">
-                            </div>
-                            <p class="skillText">Data Structures</p>
-                        </li>
-                    </div>
-                    <div class="poopy">
-                        <li class="wholeIcon">
-                            <div class="picture">
-                                <i class="devicon-arduino-plain"></i>
-                            </div>
-                            <p class="skillText">Arduino</p>
-                        </li>
-                    </div>
-                    <div class="poopy">
-                        <li class="wholeIcon">
-                            <div class="picture">
-                                <i class="devicon-rust-plain"></i>
-                            </div>
-                            <p class="skillText">Rust</p>
-                        </li>
-                    </div>
-                    <div class="poopy">
-                        <li class="wholeIcon">
-                            <div class="picture">
-                                <i class="devicon-svelte-plain"></i>
-                            </div>
-                            <p class="skillText">Svelte</p>
-                        </li>
-                    </div>
+                    {#each knownSkills as {name, icon}}
+
+                        <div class="poopy">
+                            <li class="wholeIcon">
+                                <div class="picture">
+                                    {#if icon.startsWith('devicon')}
+                                        <i class={icon}></i>
+                                    {:else}
+                                        <img src='/{icon}.svg' alt="" id="toobig">
+                                    {/if}
+                                </div>
+                                <p class="skillText">{name}</p>
+                            </li>
+                        </div>
+                    {/each}
                 </ul>
                 <p>Learning</p>
-                    <ul class="skillList">
+                <ul class="skillList">
+                    {#each learningSkills as {name, icon}}
                         <div class="poopy">
                             <li class="wholeIcon">
                                 <div class="picture">
-                                    <i class="devicon-python-plain"></i>
+                                    {#if icon.startsWith('devicon')}
+                                        <i class={icon}></i>
+                                    {:else}
+                                        <img src='/{icon}.svg' alt="" id="toobig">
+                                    {/if}
                                 </div>
-                                <p class="skillText">Python</p>
+                                <p class="skillText">{name}</p>
                             </li>
                         </div>
-                        <!-- Repeat this for Verilog, MATLAB, Typescript, HTML, CSS, Svelte, Git, Rust -->
-                        
-                        <div class="poopy">
-                            <li class="wholeIcon">
-                                <div class="picture">
-                                    <img src="/operating-systems.svg" alt="datas" id="toobig">
-                                </div>
-                                <p class="skillText">OS</p>
-                            </li>
-                        </div>
-                        <div class="poopy">
-                            <li class="wholeIcon">
-                                <div class="picture">
-                                    <img src="/computer-arch.svg" alt="datas" id="toobig">
-                                </div>
-                                <p class="skillText">Computer Architecture</p>
-                            </li>
-                        </div>
-                        <div class="poopy">
-                            <li class="wholeIcon">
-                                <div class="picture">
-                                    <img src="/statistics.svg" alt="datas" id="toobig">
-                                </div>
-                                <p class="skillText">Statistics</p>
-                            </li>
-                        </div>
-                        <div class="poopy">
-                            <li class="wholeIcon">
-                                <div class="picture">
-                                    <img src="/machine-learning.svg" alt="datas" id="toobig">
-                                </div>
-                                <p class="skillText">Machine Learning</p>
-                            </li>
-                        </div>
-                        <div class="poopy">
-                            <li class="wholeIcon">
-                                <div class="picture">
-                                    <img src="/deep-learning-project-svgrepo-com.svg" alt="datas" id="toobig">
-                                </div>
-                                <p class="skillText">Deep Learning</p>
-                            </li>
-                        </div>
-                        <div class="poopy">
-                            <li class="wholeIcon">
-                                <div class="picture">
-                                    <img src="/eye.svg" alt="datas" id="toobig">
-                                </div>
-                                <p class="skillText">Computer Vision</p>
-                            </li>
-                        </div>
+                    {/each}
                     </ul>
 </section>
